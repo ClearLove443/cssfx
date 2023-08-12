2f54deff82c3172eda97705a8745607643b14b8b
ClearLove443 <2029336036@qq.com>
2023-08-13 01:03:19 +0800

    update main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,18 @@
 import hljsVuePlugin from '@highlightjs/vue-plugin';
-import 'highlight.js/lib/common';
+import hljs from 'highlight.js/lib/core';
+import css from 'highlight.js/lib/languages/css';
+import javascript from 'highlight.js/lib/languages/javascript';
+import xml from 'highlight.js/lib/languages/xml';
 import 'highlight.js/styles/atom-one-dark.css';
 import clipboard from 'v-clipboard';
 import { createApp } from 'vue';
 import App from './App.vue';
 import './assets/main.css';
 import './style.css';
+
+hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('css', css);
+hljs.registerLanguage('xml', xml);
 
 const app = createApp(App)
 
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/cssfx/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/cssfx/' : './',
   plugins: [vue()],
 })
